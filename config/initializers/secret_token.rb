d310fb8bd5db73a31b8eabc7ea9e9ec0fb74b743
QuantumVenturess <quantumventuress@gmail.com>
2013-05-06 12:33:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abiteapp::Application.config.secret_token = '405784d1a9e90cce7c3d40f522dda6c7240d84a94c3f7107b67a0626748ffc5be54783fb721c8d79d6cc972eb8793870c02a2f191cfb740a0d17cdfc7ac12c3f'
