0343b546aec230b5fa2fe5c6b0acafe96e094aa8
QuantumVenturess <quantumventuress@gmail.com>
2013-05-10 10:28:14 -0700

    created custom lazy loading of image

--- app/assets/javascripts/lazy.js
+++ app/assets/javascripts/lazy.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
 		failure_limit: 50,
     skip_invisible: false
 	});
-	$('.lazyLoad').lazyload({
-		event: 'load',
-		failure_limit: 50
-	});
+  $('.lazyLoad').each(function() {
+    var original = $(this).attr('data-original');
+    $(this).attr('src', original);
+  })
+  /*
+  	$('.lazyLoad').lazyload({
+  		event: 'load',
+  		failure_limit: 50
+  	});
+  */
 });
