7c62f2a8001063f39a97f7394b6d80d4c3a2eacf
QuantumVenturess <quantumventuress@gmail.com>
2013-05-14 12:02:22 -0700

    re-add tutorial; added yelp logo at the bottom of start place results; added yelp star review image for all reviews

--- app/helpers/yelp_helper.rb
+++ app/helpers/yelp_helper.rb
@@ -16,6 +16,31 @@ module YelpHelper
     'san jose'
   end
 
+  def yelp_rating_to_class(rating)
+    r = rating.to_f
+    if r == 0.0
+      'zero'
+    elsif r == 1.0
+      'one'
+    elsif r == 1.5
+      'onePointFive'
+    elsif r == 2.0
+      'two'
+    elsif r == 2.5
+      'twoPointFive'
+    elsif r == 3.0
+      'three'
+    elsif r == 3.5
+      'threePointFive'
+    elsif r == 4.0
+      'four'
+    elsif r == 4.5
+      'fourPointFive'
+    elsif r == 5.0
+      'five'
+    end
+  end
+
   def yelp_token
     'Z00lrMFwppxUcD0Y2xLKBrxqQaS88cl4'
   end
